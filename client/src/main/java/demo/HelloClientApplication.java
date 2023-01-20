1b903a3ff5ab91c5b59423433b567e927ff248a9
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2023-01-20 16:26:56 +0100

    Add validation sample.

--- client/src/main/java/demo/HelloClientApplication.java
+++ client/src/main/java/demo/HelloClientApplication.java
@@ -1,12 +1,17 @@
 package demo;
 
+import jakarta.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -19,6 +24,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 @EnableFeignClients
 public class HelloClientApplication {
+
 	@Autowired
 	HelloClient client;
 
@@ -27,13 +33,23 @@ public class HelloClientApplication {
 		return client.hello();
 	}
 
+	@RequestMapping(value = "/test", method = GET)
+	String test() {
+		return client.test(null);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloClientApplication.class, args);
 	}
 
 	@FeignClient("HelloServer")
+	@Validated
 	interface HelloClient {
-		@RequestMapping(value = "/", method = GET)
+
+		@GetMapping(value = "/")
 		String hello();
+
+		@GetMapping(value = "/test")
+		String test(@RequestParam("message") @NotNull String message);
 	}
 }
--- server/src/main/java/demo/HelloServerApplication.java
+++ server/src/main/java/demo/HelloServerApplication.java
@@ -1,15 +1,13 @@
 package demo;
 
-import java.util.List;
-import java.util.Random;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,12 +22,14 @@ public class HelloServerApplication {
 
 	@RequestMapping("/")
 	public String hello() {
-		List<ServiceInstance> instances = client.getInstances("HelloServer");
-		ServiceInstance selectedInstance = instances
-				.get(new Random().nextInt(instances.size()));
-		return "Hello World: " + selectedInstance.getServiceId() + ":" + selectedInstance
-				.getHost() + ":" + selectedInstance.getPort();
+		return "Hello!";
 	}
+
+	@GetMapping("/test")
+	String test(@RequestParam(required = false) String test) {
+		return test;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloServerApplication.class, args);
